eb44f6f24dd5515902059c56f01a841185e5d6ca
ashwin37 <ashwint379@gmail.com>
2017-02-10 01:15:10 -0500

    A Sample Scroll effect with page control effect for an image !!

--- Image Scroll/ViewController.swift
+++ Image Scroll/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var imageArray = [UIImage]()  // Array
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        scrollView.frame = view.frame
+        imageArray = [#imageLiteral(resourceName: "rio-de-janeiro-thumb"), #imageLiteral(resourceName: "new-york-city-thumb"), #imageLiteral(resourceName: "barcelona-thumb"), #imageLiteral(resourceName: "london-thumb"), #imageLiteral(resourceName: "paris-thumb")]
+        
+       // image literal was selected in the above code.
+        
+        for i in 0..<imageArray.count
+        {
+         let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleAspectFit
+            let xPostion = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPostion, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+        
+        scrollView.contentSize.width = scrollView.frame.width * CGFloat(i + 1)
+            scrollView.addSubview(imageView)
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
 
